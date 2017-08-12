e13f305c41c18b2a05d61ff7e24055091b75e583
Divyank Karolia <divyankkarolia97@gmail.com>
2017-08-12 16:50:07 +0530

    successfully configured postgres

--- config.js
+++ config.js
@@ -1,15 +1,17 @@
 const DB ={
 
-    DATABASE:"",
-    USERNAME:"",
-    PASSWORD:""
+    DATABASE:"practiceDB",
+    USERNAME:"divyank",
+    PASSWORD:"dkarolia"
 
 }
 
 const CONFIG ={
-    DATABASE_URL: process.env.DATABASE_URL | `postgres://${DB.USERNAME}:${DB.PASSWORD}@localhost:5432/${DB.DATABASE}`,
+    DATABASE_URL: process.env.DATABASE_URL || `postgres://${DB.USERNAME}:${DB.PASSWORD}@localhost:5432/${DB.DATABASE}`,
     SERVER_PORT: process.env.PORT | 1234
 }
+console.log(CONFIG);
+
 
 
 module.exports = CONFIG;
